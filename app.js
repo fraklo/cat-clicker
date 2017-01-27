dcd512bc3910959460cd400a2b04618f1546c3a8
fraklo <fraklopez@gmail.com>
2017-01-26 23:31:23 -0800

    add admin functionality

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 +function() {
+	const utils = {
+		getFormData: form => {
+			const els = this.form.children;
+			return Array.from(els).reduce( (data, el) => {
+				if(el.name) {
+					data[el.name] = el.value;
+				}
+				return data;
+			}, {});
+		}
+	}
+
 	const catData = {
 		currentCat: null,
 		cats: [
@@ -25,8 +37,57 @@
 		]
 	}
 
+	const adminView = {
+		init: () => {
+			this.adminSection = document.getElementById('admin-section');
+			this.adminSection.addEventListener('click', () => {
+				if(event.target.classList.contains('admin-toggle')) {
+					adminView.toggle();
+				}
+			})
+			this.form = document.forms.namedItem('admin-form');
+			this.form.addEventListener('submit', adminView.handleSubmit);
+			document.addEventListener('loadCat', adminView.updateFormData);
+			document.addEventListener('counterIncremented', adminView.updateCounter);
+		},
+
+		handleSubmit: event => {
+			event.preventDefault();
+			controller.updateCurrentCat(utils.getFormData(this.form));
+			adminView.toggle();
+		},
+
+		toggle: () => {
+			this.adminSection.classList.toggle('active');
+		},
+
+		updateCounter: () => {
+			const counter = Array.from(this.form.children).filter( input => input.name == 'clickCount' )[0];
+			const cat = controller.getCurrentCat();
+			counter.value = cat.clickCount;
+		},
+
+		updateFormData: () => {
+			const els = this.form.children;
+			let el;
+			const cat = controller.getCurrentCat();
+			for(let i = 0; i < els.length; i++) {
+				el = els[i];
+				if(el.name) {
+					el.value = cat[el.name];
+				}
+			}
+		}
+	}
+
 	const listView = {
+		init: () => {
+			this.catList = document.getElementById('cat-list');
+			listView.render();
+		},
+
 		render: () => {
+			this.catList.innerHTML = "";
 			const fragment = document.createDocumentFragment();
 			controller.getCats().forEach((cat, i) => {
 				const li = document.createElement('li');
@@ -36,21 +97,22 @@
 				})
 				fragment.appendChild(li);
 			})
-			const catList = document.getElementById('cat-list');
-			catList.appendChild(fragment);
+			this.catList.appendChild(fragment);
+			this.catList.children = this.catList.children[0];
 		}
 	}
 
 	const catView = {
 		init: () => {
-			this.catShow = document.getElementById('cat-show');
-			this.img = this.catShow.getElementsByClassName('cat-image')[0];
+			this.catSection = document.getElementById('cat-section');
+			this.img = this.catSection.getElementsByClassName('cat-image')[0];
 			this.img.addEventListener('click', event => {
 				controller.incrementClickCount();
 			})
-			this.title = this.catShow.getElementsByClassName('cat-title')[0];
-			this.counter = this.catShow.getElementsByClassName('click-count')[0];
-			catView.render();
+			this.title = this.catSection.getElementsByClassName('cat-title')[0];
+			this.counter = this.catSection.getElementsByClassName('click-count')[0];
+			document.addEventListener('loadCat', catView.render);
+			document.addEventListener('counterIncremented', catView.updateCounter);
 		},
 
 		render: () => {
@@ -69,8 +131,10 @@
 	const controller = {
 		init: () => {
 			controller.setCurrentCat(0);
-			listView.render();
+			adminView.init();
+			listView.init();
 			catView.init();
+			controller.triggerData('loadCat');
 		},
 
 		getCats: () => 
@@ -81,17 +145,27 @@
 
 		loadCat: cat => {
 			controller.setCurrentCat(cat);
-			catView.render();
+			controller.triggerData('loadCat');
 		},
 
 		incrementClickCount: () => {
 			const cat = controller.getCurrentCat();
 			cat.clickCount++;
-			catView.updateCounter();
+			controller.triggerData('counterIncremented');
 		},
 
 		setCurrentCat: cat => 
-			catData.currentCat = cat
+			catData.currentCat = cat,
+
+		triggerData: event => {
+			document.dispatchEvent(new Event(event));
+		},
+
+		updateCurrentCat: newCat => {
+			catData.cats[catData.currentCat] = newCat;
+			listView.render();
+			controller.triggerData('loadCat');
+		}
 	}
 
 	controller.init();
